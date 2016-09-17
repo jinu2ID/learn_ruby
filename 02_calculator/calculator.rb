59f931b41d876a85429205abe16b9fc7c548a821
Jinu <jinu.jacob90@gmail.com>
2016-09-17 14:49:58 -0400

    finished add, subtract, and sum in 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
-#write your code here
+def add(a,b)
+ a + b
+end
+
+def subtract(a,b)
+  a - b
+end
+
+def sum(array)
+  if array.empty?
+    return 0
+  else 
+    array[0] + sum(array[1..-1])
+  end
+
+end
