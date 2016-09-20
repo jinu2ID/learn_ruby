46689ee0c243dc8f72c9a7f41ec28f5bef3b4a06
Jinu <jinu.jacob90@gmail.com>
2016-09-19 20:04:32 -0400

    finished multiply, power, and factorial; added tests for each

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -14,3 +14,39 @@ def sum(array)
   end
 
 end
+
+def multiply(*args)
+  if args.empty?
+    return 0
+  else
+    product = 1
+    args.each do |var|
+      product = product * var
+    end
+    return product
+  end
+end
+
+def power(a,b)
+  if b == 0
+    return 1
+  elsif b == 1
+    return a
+  else
+    c = a 
+    for i in 2..b
+      c = c * a
+    end
+    return c
+  end
+end
+
+def factorial(a)
+  if a == 0
+    return 1
+  elsif a == 1
+    return 1
+  else
+    return a * factorial(a-1)
+  end
+end
