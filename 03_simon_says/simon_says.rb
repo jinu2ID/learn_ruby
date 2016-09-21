49f4701875a674c8577b9f4d4df472a190a46db6
Jinu <jinu.jacob90@gmail.com>
2016-09-20 21:56:17 -0400

    finished 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,40 @@
-#write your code here
+def echo(arg)
+  arg
+end
+
+def shout(arg)
+  arg.upcase
+end
+
+def repeat(arg, times=2)
+  output = arg
+  for i in 1..times-1
+    output = output + " " + arg
+  end
+  return output
+end
+
+def start_of_word(arg, n=1)
+  arg[0..n-1]
+end
+
+def first_word(arg)
+  arg.split()[0]  
+end
+
+def titleize(arg)
+  small_words = ["and", "the", "over"]
+  capitalized = []
+
+  arg.split().each do |word|
+    if !(small_words.include?(word))
+       word.capitalize!()
+    end
+    capitalized << word
+  end
+
+  capitalized[0].capitalize!()
+  capitalized.join(" ")
+end
+
+
